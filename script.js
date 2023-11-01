a6cc164e1bddf6e821a3b985638f13250dd85ef0
séverine zapata <severine.zapata@orange.fr>
2023-11-01 14:46:10 +0100

    three commit

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let i = "";
 function FizzBuzz(i) {
+  let outputDiv = document.getElementById("output");
   for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       console.log("FizzBuzz");
+      outputDiv.innerHTML += "FizzBuzz<br>";
     } else {
       if (i % 5 == 0) {
         console.log("Buzz");
+        outputDiv.innerHTML += "Buzz<br>";
       } else {
         if (i % 3 == 0) {
           console.log("Fizz");
+          outputDiv.innerHTML += "Fizz<br>";
         } else {
           console.log(i);
+          outputDiv.innerHTML += i + "<br>";
         }
       }
     }
   }
 }
+console.log(FizzBuzz(i));
